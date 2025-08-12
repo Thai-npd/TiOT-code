73375c267e4bed0d52128c4beca06a5700bf25f7
Thai-pdn <thai.pdn@gmail.com>
2025-08-12 02:08:59 +0000

    from remote: update knn

--- kNN_TiOT.py
+++ kNN_TiOT.py
@@ -124,37 +124,38 @@ def read_result(result_file):
 
 def experiment_kNN(dataset_name, w_TAOT, RUN = True):
     eps_list = [0.01*i for i in range(1,11)]
-    eps_list = [0.01*i for i in range(7,11)]
     eps_name = f" ({eps_list[0]} to {eps_list[-1]})"       
     plot_file = os.path.join("kNN_data","plots", "Comparison on " + dataset_name + eps_name + "__test" + ".pdf")
     result_file = os.path.join("kNN_data", "saved_results","Results on " + dataset_name + eps_name + "__test" + '.csv')
     if RUN :
         data = process_data(dataset_name= dataset_name)
         w_list = [ round(w_TAOT/5, 3), w_TAOT,w_TAOT*5]
-        alg_names = ["eTiOT", 'eTAOT']  +  [f"eTAOT(w = {w})" for w in w_list]
+        alg_names = ["eTiOT", 'eTAOT(w = w*)']  +  [f"eTAOT(w = {w})" for w in w_list]
+        alg_names = ['eTAOT(w = w*)']
         results = {**{'eps': eps_list}, **{name: [] for name in alg_names}}
         w_opt = get_w_opt(data[0], data[1], eps=0.01)
         for eps in eps_list:
-            results['eTiOT'].append(kNN(dataset_name, data, metric_name='eTiOT', eps = eps, w = None))
-            results[f"eTAOT"].append(kNN(dataset_name, data, metric_name='eTAOT', eps = eps, w = w_opt))
-            for w in w_list:
-                results[f"eTAOT(w = {w})"].append(kNN(dataset_name, data, metric_name='oriTAOT', eps = eps, w = w))
+            #results['eTiOT'].append(kNN(dataset_name, data, metric_name='eTiOT', eps = eps, w = None))
+            results["eTAOT(w = w*)"].append(kNN(dataset_name, data, metric_name='eTAOT', eps = eps, w = w_opt))
+            #for w in w_list:
+            #    results[f"eTAOT(w = {w})"].append(kNN(dataset_name, data, metric_name='oriTAOT', eps = eps, w = w))
 
         save_result(results, result_file)
         plot_results(results, plot_file)
     else:
         results = read_result(result_file)
+        save_result(results, result_file)
         plot_results(results, plot_file)
  
 if __name__ == "__main__":
     # ===> Tier 1 
 
-    # experiment_kNN("SonyAIBORobotSurface1", 2)
-    # experiment_kNN("CBF", 1)
-    # experiment_kNN("DistalPhalanxOutlineAgeGroup", 1)
-    # experiment_kNN("ProximalPhalanxTW", 0.7)
-    # experiment_kNN('ProximalPhalanxOutlineCorrect', 0.7)
-    # experiment_kNN('MiddlePhalanxOutlineCorrect', 0.5)
+    experiment_kNN("SonyAIBORobotSurface1", 2)
+    experiment_kNN("CBF", 1)
+    experiment_kNN("DistalPhalanxOutlineAgeGroup", 1)
+    experiment_kNN("ProximalPhalanxTW", 0.7)
+    experiment_kNN('ProximalPhalanxOutlineCorrect', 0.7)
+    experiment_kNN('MiddlePhalanxOutlineCorrect', 0.5)
     
     # experiment_kNN('Adiac',0.1) --> need to re-run
     #experiment_kNN('SwedishLeaf',0.9) --> need to re-run
@@ -179,14 +180,14 @@ if __name__ == "__main__":
     # experiment_kNN('TwoLeadECG', 0.1)
     
     #experiment_kNN('SyntheticControl', 4)
-    experiment_kNN('Chinatown', 1)
-    experiment_kNN('ItalyPowerDemand', 7)
+    # experiment_kNN('Chinatown', 1)
+    # experiment_kNN('ItalyPowerDemand', 7)
 
 
     # experiment_kNN('MedicalImages', 4)
     # experiment_kNN('ArrowHead', 3)
-    experiment_kNN('ToeSegmentation2', 0.8)
-    experiment_kNN('ToeSegmentation1', 0.1)
+    # experiment_kNN('ToeSegmentation2', 0.8)
+    # experiment_kNN('ToeSegmentation1', 0.1)
     # experiment_kNN('Meat', 0.9)
     # experiment_kNN('ShapeletSim', 2)
     # experiment_kNN('DiatomSizeReduction', 0.2)
